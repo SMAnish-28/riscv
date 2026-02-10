3d5febc360b8d26f86798b025d7dd574a11b8e37
debu018-dev <debudan419@gmail.com>
2026-02-10 18:02:21 +0530

    Refactor pc_block module and remove commented code

--- pc.v
+++ pc.v
@@ -1,57 +1,3 @@
-// `timescale 1ns/1ps
-
-// module mux(pc_sel, immediate, from_alu, hold, pc_next, pc_in);
-
-// input [1:0] pc_sel;
-// input [31:0] immediate;
-// input [31:0] from_alu;
-// input [31:0] hold;
-// input [31:0] pc_next;
-// output reg [31:0] pc_in;
-
-// always @(*) begin
-//     case(pc_sel)
-//         2'b00: pc_in = immediate;
-//         2'b01: pc_in = from_alu;
-//         2'b10: pc_in = hold;
-//         2'b11: pc_in = pc_next;
-//         default: pc_in = 32'b0;
-//     endcase
-// end
-
-// endmodule
-
-// module program_counter(pc, pc_next, pc_en, clk, rst);
-
-// input clk;
-// input rst;
-// input pc_en;
-// input [31:0] pc_next;
-// output reg [31:0] pc;
-
-// initial begin
-//     pc = 32'b0;
-// end
-
-// always @(posedge clk or posedge rst) begin
-//     if (rst) begin
-//         pc <= 32'b0;
-//     end 
-//     else if (pc_en) begin
-//         pc <= pc_next;
-//     end
-// end
-
-// endmodule
-
-// module next_program_counter(pc, pc_next);
-
-// input [31:0] pc;
-// output [31:0] pc_next;
-
-// assign pc_next = pc + 32'd4;
-
-// endmodule
 
 `timescale 1ns/1ps
 
@@ -89,3 +35,4 @@ module pc_block (
     end
 
 endmodule
+
